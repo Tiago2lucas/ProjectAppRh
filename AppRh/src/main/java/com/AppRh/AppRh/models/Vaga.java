e8fe144cfa5a40430c1acedd5f343ce49e7fbfa3
Tiago2lucas <garotinhobolado.lucas@gmail.com>
2023-12-14 15:25:47 -0300

    ProjetoAppRh

---
+++ AppRh/src/main/java/com/AppRh/AppRh/models/Vaga.java
@@ -0,0 +1,17 @@
+package com.AppRh.AppRh.models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+
+
+public class Vaga {
+
+}
