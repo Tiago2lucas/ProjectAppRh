11a823a1c5a2b59dac92f746b32004e1ece0e56c
Tiago2lucas <garotinhobolado.lucas@gmail.com>
2023-12-15 15:29:37 -0300

    update

--- AppRh/src/main/java/com/AppRh/AppRh/models/Vaga.java
+++ AppRh/src/main/java/com/AppRh/AppRh/models/Vaga.java
@@ -1,17 +1,27 @@
 package com.AppRh.AppRh.models;
 
+
 import java.io.Serializable;
 import java.util.List;
+
+import javax.annotation.Generated;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
 
 
 
+public class Vaga implements Serializable {
 
-public class Vaga {
+
+	private static final long serialVersionUID = 1L ;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
 
 }
