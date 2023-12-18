854c115979084dd8d3dc23a304f3aa2b731574de
Tiago2lucas <garotinhobolado.lucas@gmail.com>
2023-12-18 15:55:41 -0300

    update

--- AppRh/src/main/java/com/AppRh/AppRh/models/Vaga.java
+++ AppRh/src/main/java/com/AppRh/AppRh/models/Vaga.java
@@ -18,10 +18,81 @@ import javax.validation.constraints.NotEmpty;
 public class Vaga implements Serializable {
 
 
+		//Att controle de versionamento 
 	private static final long serialVersionUID = 1L ;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long codigo;
 
+	@NotEmpty
+	private String nome;
+	
+	
+	@NotEmpty
+	private String descricao;
+	
+	@NotEmpty
+	private String salario;
+	
+	@NotEmpty
+	private String data;
+	
+	@OneToMany(mappedBy = "vaga", cascade = CascadeType.REMOVE)
+	private List<Candidato> candidatos;
+	
+	
+	// Getters Setters
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getSalario() {
+		return salario;
+	}
+
+	public void setSalario(String salario) {
+		this.salario = salario;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public List<Candidato> getCandidatos() {
+		return candidatos;
+	}
+
+	public void setCandidatos(List<Candidato> candidatos) {
+		this.candidatos = candidatos;
+	}	
+	
+	
+	
+	
 }
